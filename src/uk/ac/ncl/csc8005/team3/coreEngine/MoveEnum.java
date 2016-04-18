dbefb5fdd3ef7fbd7b1f5affff14e8cd8036e41e
EternalSaga <xuefengren2010@hotmail.com>
2016-04-18 11:52:58 +0100

    Add comments

--- src/uk/ac/ncl/csc8005/team3/coreEngine/MoveEnum.java
+++ src/uk/ac/ncl/csc8005/team3/coreEngine/MoveEnum.java
@@ -1,5 +1,9 @@
 package uk.ac.ncl.csc8005.team3.coreEngine;
-
+/**
+ * When players are controlling the character just add them into the move history.
+ * @author RobinLew
+ *
+ */
 public enum MoveEnum {
 	UP,DOWN,LEFT,RIGHT;
 }
--- src/uk/ac/ncl/csc8005/team3/coreEngine/MoveHistory.java
+++ src/uk/ac/ncl/csc8005/team3/coreEngine/MoveHistory.java
@@ -1,7 +1,11 @@
 package uk.ac.ncl.csc8005.team3.coreEngine;
 
 import java.util.LinkedList;
-
+/**
+ * It is only a LinkedLikst data structure to store the move history. You can use all the method of the queue to control it.
+ * @author RobinLew
+ *
+ */
 public class MoveHistory extends LinkedList<MoveEnum>{
 	
 }
