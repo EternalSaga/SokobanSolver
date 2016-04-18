49ca352b62e6b956033f37a9d01a3076b9e8af11
EternalSaga <xuefengren2010@hotmail.com>
2016-04-18 11:56:46 +0100

    Add comments.

--- src/uk/ac/ncl/csc8005/team3/coreEngine/MoveHistory.java
+++ src/uk/ac/ncl/csc8005/team3/coreEngine/MoveHistory.java
@@ -2,7 +2,9 @@ package uk.ac.ncl.csc8005.team3.coreEngine;
 
 import java.util.LinkedList;
 /**
- * It is only a LinkedLikst data structure to store the move history. You can use all the method of the queue to control it.
+ * It is only a LinkedLikst data structure to store the move history. 
+ * You can use all the method of the queue to control it.
+ * It not only can be used to store the move of character, it also can be used to store box moves.
  * @author RobinLew
  *
  */
