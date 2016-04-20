a6a5bf1808ca7285270712173aa61e794bc30147
kato-suggy <kato.suggy@gmail.com>
2016-04-20 14:24:12 +0100

    Added files via upload

---
+++ src/uk/ac/ncl/csc8005/team3/coreEngine/KeyManager.java
@@ -0,0 +1,48 @@
+package uk.ac.ncl.csc8005.team3.coreEngine;
+
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ * taken from https://github.com/CodeNMore/New-Beginner-Java-Game-Programming-Src/blob/master/Episode%2014/TileGame/src/dev/codenmore/tilegame/input/KeyManager.java
+ * 
+ * @author Kate
+ *
+ */
+
+public class KeyManager implements KeyListener {
+	
+	private boolean[] keys;
+	public boolean up, down, left, right;
+	
+	public KeyManager() {
+		keys = new boolean[256]; //i don't know why 256
+	}
+	
+	public void tick() {
+		up = keys[KeyEvent.VK_UP];
+		down = keys[KeyEvent.VK_DOWN];
+		left = keys[KeyEvent.VK_LEFT];
+		right = keys[KeyEvent.VK_RIGHT];
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		keys[e.getKeyCode()] = true;
+		System.out.println("pressed"); //for testing
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		keys[e.getKeyCode()] = false;
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
