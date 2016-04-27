e57f318b96bd8cce11b0855f523a01c4f9a19d8d
yqjapple <yqjapple@gmail.com>
2016-04-27 15:12:34 +0100

    Update Solver.java

--- src/uk/ac/ncl/csc8005/team3/GUI/Solver.java
+++ src/uk/ac/ncl/csc8005/team3/GUI/Solver.java
@@ -27,7 +27,7 @@ public class Solver extends JFrame {
 		}
 		
 		private void initialize() {
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 			setBounds(100, 100, 450, 300);
 			contentPane = new JPanel();
 			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
