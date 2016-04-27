bd9ecbff78058189c4b29ff10d5f45741d8635d3
yqjapple <yqjapple@gmail.com>
2016-04-27 11:19:44 +0100

    start the GUI

--- src/JunitTest/LoadWorld.java
+++ src/JunitTest/LoadWorld.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package JunitTest;
+package sokoban_new;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
@@ -10,10 +10,6 @@ import java.util.ArrayList;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
-import uk.ac.ncl.csc8005.team3.GUI.BoardPanel;
-import uk.ac.ncl.csc8005.team3.coreEngine.Board;
-import uk.ac.ncl.csc8005.team3.coreEngine.IOMethods;
 
 /**
  * @author yqjapple
@@ -29,7 +25,7 @@ public class LoadWorld {
 	
 		public static void main(String[] args) {
 			// test boardPanel 
-			IOMethods io = new IOMethods();
+			/*IOMethods io = new IOMethods();
 			Board level = io.loadBoardFromFile("res/levelCollection/level.txt");
 			BoardPanel boardPanel = new BoardPanel(level);
 			System.out.print(level.getBlockAttribute(2, 3));
@@ -38,13 +34,15 @@ public class LoadWorld {
 			f1.getContentPane().add(boardPanel, BorderLayout.CENTER);	
 			f1.setVisible(true);
 			f1.pack();
-			f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);*/
 			
 			// comment out line 28-37 and remove comment mark of line 41 - 43
 			// you can then get a GameWindow test
-			//FileFolder f = new FileFolder("res/levelCollection");	
-	        //GameWindow w = new GameWindow(f);
-		   // w.start();
+			FileFolder f = new FileFolder("res/levelCollection");	
+	        GameWindow w = new GameWindow(f);
+		   w.start();
+		  // Solver s = new Solver();
+		   //s.start();
 		}
 
 	
