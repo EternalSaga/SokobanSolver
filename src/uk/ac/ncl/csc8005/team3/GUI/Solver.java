a9820d34bae6603eabf5fdac7b2fff614afbd0d7
yqjapple <yqjapple@gmail.com>
2016-04-29 11:59:02 +0100

    new

--- src/uk/ac/ncl/csc8005/team3/GUI/Solver.java
+++ src/uk/ac/ncl/csc8005/team3/GUI/Solver.java
@@ -4,23 +4,25 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import uk.ac.ncl.csc8005.team3.algorithm.AStarSearch;
+import uk.ac.ncl.csc8005.team3.algorithm.Heuristics;
+import uk.ac.ncl.csc8005.team3.coreEngine.Board;
 
 public class Solver extends JFrame {
 
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
+	private Board board;
 	
 
 	/**
 	 * Create the frame.
 	 */
-	public Solver() {
-		
+	public Solver(Board board) {
+		this.board = board;
 	}
 		public void start(){
 			initialize();
@@ -29,14 +31,18 @@ public class Solver extends JFrame {
 		private void initialize() {
 		
 			setBounds(100, 100, 450, 300);
-			contentPane = new JPanel();
-			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-			contentPane.setLayout(new BorderLayout(0, 0));
-			setContentPane(contentPane);
-			setVisible(true);
-
 			
-		
+			contentPane = new JPanel();			
+			Heuristics h = new Heuristics(board.getGoals(), 'm');
+			AStarSearch as = new AStarSearch(h);
+			JLabel label1 = new JLabel("The solution for this level is:");
+			label1.setSize(50, 150);
+			JLabel label2 = new JLabel(as.prioritySearch(board));
+			label2.setBounds(50,50,225,150);
+			getContentPane().add(label1, BorderLayout.NORTH);
+			getContentPane().add(label2, BorderLayout.CENTER);
+			pack();
+			setVisible(true);
 	}
 
 }
