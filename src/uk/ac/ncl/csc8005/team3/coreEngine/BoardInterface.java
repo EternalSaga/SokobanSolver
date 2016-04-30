589bedb325306c6540a43fd6bd6273f9abc77747
MMurray112 <marie-claremurray@hotmail.co.uk>
2016-04-30 11:49:17 +0100

    Update BoardInterface.java

--- src/uk/ac/ncl/csc8005/team3/coreEngine/BoardInterface.java
+++ src/uk/ac/ncl/csc8005/team3/coreEngine/BoardInterface.java
@@ -1,5 +1,29 @@
 package uk.ac.ncl.csc8005.team3.coreEngine;
 
 public interface BoardInterface {
+  
+  public void addToMap(Coordinate thisCoordinate, BlockAttribute thisBlockAttribute);
+  
+  public Coordinate getPlayerPosition();
+  
+  public int getNumberOfGoals();
+  
+  public void clearMap();
+  
+  public BlockAttribute getBlockAttribute(int x, int y);
+  
+  public void addRow(String row);
+  
+  public int getWidth();
+  
+  public int getHeight();
+  
+  public BlockAttribute setEnum(char ch);
+  
+	public void setCell();
+	
+	public String getRow(int i);
+	
+	public Map<Coordinate, BlockAttribute> getTreeMap();
 
 }
