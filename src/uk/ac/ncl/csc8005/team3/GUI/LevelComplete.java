02ce19017e5a4e8e3a317ad6aef04a99e0a839cb
yqjapple <yqjapple@gmail.com>
2016-05-02 17:37:22 +0100

    organised the content

--- src/uk/ac/ncl/csc8005/team3/GUI/LevelComplete.java
+++ src/uk/ac/ncl/csc8005/team3/GUI/LevelComplete.java
@@ -1,11 +1,13 @@
+/**
+ * LevelComplete class shows a dialog window to tell users they have completed the level
+ * and ask they whether they want to continue to play the game.
+ * @author: Qijing Yu
+ */ 
 package uk.ac.ncl.csc8005.team3.GUI;
 
 import java.awt.BorderLayout;
-
-
 import javax.swing.JDialog;
 import javax.swing.JFrame;
-
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
@@ -16,12 +18,7 @@ public class LevelComplete extends JFrame {
 	private JDialog d;
 	private JOptionPane o;
 	private FileFolder f;
-	
 
-
-	/**
-	 * Create the frame.
-	 */
 	public LevelComplete(FileFolder f) {
 		d = new JDialog();
 		o = new JOptionPane();
@@ -35,10 +32,7 @@ public class LevelComplete extends JFrame {
 	
 	public void initialize(){
 		
-        
-		
-		
-		d.setDefaultLookAndFeelDecorated(true);
+        	d.setDefaultLookAndFeelDecorated(true);
 		int response = o.showConfirmDialog(null, "Do you wish to continue?", "Wow! Level Completed!", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
 		if (response == o.NO_OPTION){
 			System.exit(0);} 
