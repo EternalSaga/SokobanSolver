7bb3d7099ed7233b523c145bf9317bb1b8ad8c99
yqjapple <yqjapple@gmail.com>
2016-05-02 16:55:19 +0100

    organised the content

--- src/JunitTest/LoadWorld.java
+++ src/JunitTest/LoadWorld.java
@@ -1,53 +1,23 @@
 /**
- * 
+ * @author Qijing Yu
+ *This is the class to launch the Game.
  */
 package JunitTest;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.util.ArrayList;
-
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 import uk.ac.ncl.csc8005.team3.GUI.FileFolder;
 import uk.ac.ncl.csc8005.team3.GUI.GameWindow;
 
-/**
- * @author yqjapple
- *
- */
 
-/**
- * Launch the application.
- */
+
 
 public class LoadWorld {
 
 	
 		public static void main(String[] args) {
-			// test boardPanel 
-			/*IOMethods io = new IOMethods();
-			Board level = io.loadBoardFromFile("res/levelCollection/level.txt");
-			BoardPanel boardPanel = new BoardPanel(level);
-			System.out.print(level.getBlockAttribute(2, 3));
-			
-		    JFrame f1 = new JFrame();
-			f1.getContentPane().add(boardPanel, BorderLayout.CENTER);	
-			f1.setVisible(true);
-			f1.pack();
-			f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);*/
-			
-			// comment out line 28-37 and remove comment mark of line 41 - 43
-			// you can then get a GameWindow test
-			FileFolder f = new FileFolder("resources/levelCollection");	
+		//load the game from the default folder.
+		FileFolder f = new FileFolder("resources/levelCollection");	
 	        GameWindow w = new GameWindow(f);
-		   w.start();
-		  //Solver s = new Solver();
-		   //s.start();
+	        w.start();
 		}
-
-	
-
 }
